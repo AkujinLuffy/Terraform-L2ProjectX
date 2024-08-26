0059275624bc113b17f18a52d8eecb2a8329ab40
AkujinLuffy <devrinokumura@abv.bg>
2024-08-26 17:43:41 +0300

    Update variables.tf

--- modules/vms/variables.tf
+++ modules/vms/variables.tf
@@ -1,3 +1,19 @@
-variable "resource_group_name" {}
-variable "location" {}
-variable "subnet_id" {}
+variable "project_name" {
+  description = "The name of the project"
+  type        = string
+}
+
+variable "network_name" {
+  description = "The name of the virtual network"
+  type        = string
+}
+
+variable "public_ip" {
+  description = "The public IP address"
+  type        = string
+}
+
+variable "security_rule_id" {
+  description = "The security rule ID for the firewall"
+  type        = string
+}
